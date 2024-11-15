c91869277f12767dfc691710e58e2935d4d78db9
Mildred Nataly <mildrednatalys@gmail.com>
2024-11-15 17:28:18 -0600

    Create button to get in touch

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Button.scss'
+
+export const Button = () => {
+  return (
+    <div class="btn">
+      <a href="#">
+        GET IN TOUCH
+      </a>
+    </div>
+  )
+}
