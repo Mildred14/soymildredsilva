bdce2f288e43867dfaeb7017862136b22d9fc98c
Mildred Nataly <mildrednatalys@gmail.com>
2024-11-27 18:16:47 -0600

    Include tests for landing, categories, modal and cards

---
+++ src/views/Categories/__test__/Categories.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Categories } from '../Categories'
+
+test('display all categories',  () => {
+  render(<Categories />)
+
+  expect(screen.getAllByText('Companies')[0]).toBeInTheDocument()
+  expect(screen.getAllByText('Technology Stack')[0]).toBeInTheDocument()
+  expect(screen.getAllByText('Contact')[0]).toBeInTheDocument()
+  expect(screen.getAllByText('Curious Facts')[0]).toBeInTheDocument()
+  expect(screen.getAllByText('About me')[0]).toBeInTheDocument()
+  expect(screen.getAllByText('Reviews')[0]).toBeInTheDocument()
+})
+
+test('open Contact modal',  () => {
+  render(<Categories />)
+
+  const contact = screen.getAllByTitle('more-info')
+  fireEvent.click(contact[2])
+  expect(screen.getByTitle('modal-2')).toBeInTheDocument()
+  expect(screen.getAllByText('Contact')).toHaveLength(2)
+})
