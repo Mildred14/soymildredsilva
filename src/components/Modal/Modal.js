a5909b6f260692a9cf2f9d8d18d27852d7abd350
Mildred Nataly <mildrednatalys@gmail.com>
2024-12-04 13:54:07 -0600

    Create Work Experience content for modal, add tests and scss

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React from "react"
 import plusIcon from '../../assets/images/plus.svg'
+import { WorkExperience } from "../WorkExperience/WorkExperience"
 import "./Modal.scss"
 
 export const Modal = ({name, id, selected, status}) => {
@@ -8,6 +9,18 @@ export const Modal = ({name, id, selected, status}) => {
     categories.classList.remove("overlap-show")
     status(false)
     selected('')
+  }
+  const modalContent = () => {
+    switch(id) {
+      case 0: return(<WorkExperience />);
+      case 1: return(<h1>1</h1>);
+      case 2: return(<h1>2</h1>);
+      case 3: return(<h1>3</h1>);
+      case 4: return(<h1>4</h1>);
+      case 5: return(<h1>5</h1>);
+      default:
+        return null
+    }
   }
 
   return (
@@ -18,6 +31,7 @@ export const Modal = ({name, id, selected, status}) => {
           <img src={plusIcon} width="30" />
         </a>
       </div>
+      {modalContent()}
     </div>
   )
 }
