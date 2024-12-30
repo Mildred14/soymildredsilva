b7bab9262c9329138ec72a9c64322e1346908bba
Mildred Nataly <mildrednatalys@gmail.com>
2024-12-30 16:26:43 -0600

    Updates

---
+++ precache-manifest.ade0bf04d7b54290324c982d103fc870.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac42e53355e2b2384e06928baf94b27",
+    "url": "/soymildredsilva/index.html"
+  },
+  {
+    "revision": "7a2ed803c831db369e6b",
+    "url": "/soymildredsilva/static/css/main.47ce0c78.chunk.css"
+  },
+  {
+    "revision": "e284816e6606b0744125",
+    "url": "/soymildredsilva/static/js/2.1165117e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/soymildredsilva/static/js/2.1165117e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcaa15a394b7d73b7086",
+    "url": "/soymildredsilva/static/js/3.934fb115.chunk.js"
+  },
+  {
+    "revision": "7a2ed803c831db369e6b",
+    "url": "/soymildredsilva/static/js/main.ae3f8588.chunk.js"
+  },
+  {
+    "revision": "ed5591acec80d0278a83",
+    "url": "/soymildredsilva/static/js/runtime-main.99ea6b21.js"
+  },
+  {
+    "revision": "2b24116cee592064acae1a9d0e8017a3",
+    "url": "/soymildredsilva/static/media/Berlinerins-Trial-Grotesk.2b24116c.otf"
+  },
+  {
+    "revision": "fcd427f1ebeea0bc781d457bd3a15c74",
+    "url": "/soymildredsilva/static/media/Hagrid-Regular-trial.fcd427f1.ttf"
+  },
+  {
+    "revision": "113863faf3ef54884383c966bb3bc956",
+    "url": "/soymildredsilva/static/media/Hagrid-Text-Extrabold-trial.113863fa.ttf"
+  },
+  {
+    "revision": "8a85751ada61620d911496e1bc5b6a45",
+    "url": "/soymildredsilva/static/media/about.8a85751a.svg"
+  },
+  {
+    "revision": "98a5e5c666faac1d4d611d395fb3dfdf",
+    "url": "/soymildredsilva/static/media/companies.98a5e5c6.svg"
+  },
+  {
+    "revision": "7dd56a15b9556886523e7d0185ed6b5e",
+    "url": "/soymildredsilva/static/media/contact.7dd56a15.svg"
+  },
+  {
+    "revision": "fb6919eae2528dd684bd0d86c4caf968",
+    "url": "/soymildredsilva/static/media/css.fb6919ea.svg"
+  },
+  {
+    "revision": "efd0e2ff8a69399f570ab211f299c92d",
+    "url": "/soymildredsilva/static/media/curious-facts.efd0e2ff.svg"
+  },
+  {
+    "revision": "97b272c3182e52182ce5244161875fa6",
+    "url": "/soymildredsilva/static/media/email-icon.97b272c3.svg"
+  },
+  {
+    "revision": "60c3bad672544c33b139a3f2160307f6",
+    "url": "/soymildredsilva/static/media/figma.60c3bad6.svg"
+  },
+  {
+    "revision": "7a07f0905574e4f69ae025676f9766ba",
+    "url": "/soymildredsilva/static/media/git.7a07f090.svg"
+  },
+  {
+    "revision": "1b811051e8570e224d135075e57e3ccf",
+    "url": "/soymildredsilva/static/media/html.1b811051.svg"
+  },
+  {
+    "revision": "06fe211a851a959b5aca07707a16536c",
+    "url": "/soymildredsilva/static/media/icon.06fe211a.svg"
+  },
+  {
+    "revision": "55d83985b46d5336f45f27036b3018cd",
+    "url": "/soymildredsilva/static/media/instagram.55d83985.svg"
+  },
+  {
+    "revision": "bfb3d0854272511bcd293187199e7517",
+    "url": "/soymildredsilva/static/media/paper-airplane.bfb3d085.svg"
+  },
+  {
+    "revision": "4a745e153ca94a28d95358041522c576",
+    "url": "/soymildredsilva/static/media/plus.4a745e15.svg"
+  },
+  {
+    "revision": "c6c9894d1528328df1657511e2c27483",
+    "url": "/soymildredsilva/static/media/react.c6c9894d.svg"
+  },
+  {
+    "revision": "dbff7fe9ba98b0a18827fe530ecefadb",
+    "url": "/soymildredsilva/static/media/reviews.dbff7fe9.svg"
+  },
+  {
+    "revision": "36fd12af38ef3210953b8b865d12bf73",
+    "url": "/soymildredsilva/static/media/rubyonrails.36fd12af.svg"
+  },
+  {
+    "revision": "c0babe637979872152969e5d6155d4ac",
+    "url": "/soymildredsilva/static/media/salesloft.c0babe63.png"
+  },
+  {
+    "revision": "742d290237bf2f30dbb716d5848cf9d5",
+    "url": "/soymildredsilva/static/media/tech-stack.742d2902.svg"
+  },
+  {
+    "revision": "042bcd4c355abfca5fa70d0034af7169",
+    "url": "/soymildredsilva/static/media/typescript.042bcd4c.svg"
+  }
+]);
