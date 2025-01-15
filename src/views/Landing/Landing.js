1b0096cc8c627dbfbf74ea1783c970c27753ee77
Mildred Nataly <mildrednatalys@gmail.com>
2025-01-14 18:59:19 -0600

    - Remove unnecesary fonts - Inclue new fonts - Modify landing page with a new design - Use data for landin page

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Button } from '../../components/Button/Button'
 import { Categories } from '../Categories/Categories'
-import paperAirplane from '../../assets/images/paper-airplane.svg'
 import iconPaper from '../../assets/images/icon.svg'
 import'./Landing.scss'
 
@@ -11,17 +9,36 @@ export const Landing = () => {
       <div className='landing-page'>
         <div className="header">
           <img src={iconPaper} />
-          <p className="header__secondary-font">Mildred <br/> Silva</p>
+          <p className="header__secondary-font">MILDRED <br/> SILVA</p>
         </div>
         <div className="landing-page__content">
           <div className='lading-page__text'>
+            <h2 className='emoji'>👩🏽‍💻</h2>
             <h2>Transforming ideas</h2>
             <h2>into great apps!</h2>
-            <h2 className='secondary-font'>Code 100% made in Mexico</h2>
-            <Button />
+            <h2 className='secondary-font'>CODE 100% MADE IN MEXICO</h2>
           </div>
-          <div className="lading-page__paper-ariplane">
-            <img src={paperAirplane} className="paper-ariplane"/>
+          <div className='wrap-data'>
+            <div className='data'>
+              <div>
+                <p className='value'>4</p>
+                <p className='datum'>companies</p>
+              </div>
+              <div>
+                <p className='value'>6</p>
+                <p className='datum'>big projects</p>
+              </div>
+            </div>
+            <div className='data'>
+              <div>
+                <p className='value'>5</p>
+                <p className='datum'>years experience</p>
+              </div>
+              <div>
+                <p className='value'>+150k</p>
+                <p className='datum'>users satisfied</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
