45cbcd22cb4f5f20698828aa1c885b37af4ada1c
Mildred Nataly <mildrednatalys@gmail.com>
2025-01-21 14:09:14 -0600

    Add testimonial data, add images, add function of read more/read less, display testimonies in the different devices

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,48 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import Quotes from '../../assets/images/quotes.svg'
-import YMLData from './recommendation.yml'
+import data from './recommendation.json'
 import './Testimonials.scss'
+import naomi from '../../assets/images/naomi.jpeg'
+import wade from '../../assets/images/wade.jpeg'
+import david from '../../assets/images/david.jpeg'
+import cesar from '../../assets/images/cesar.jpeg'
+import mieko from '../../assets/images/mieko.jpeg'
 
 export const Testimonials = () => {
-  const colleagues = [
-    {
-      name: "Naomi Pohl",
-      jobPosition: "Senior Software Engineer",
-      recommendation: YMLData.recommendation.naomi
-    },
-    {
-      name: "Wade Hundley",
-      jobPosition: "Senior Engineering Manager",
-      recommendation: YMLData.recommendation.wade
-    },
-    {
-      name: "David Colin",
-      jobPosition: "Engineering Manager",
-      recommendation: YMLData.recommendation.david
-    },
-    {
-      name: "Cesar Sanabria",
-      jobPosition: "Director of Engineering & Site Lead",
-      recommendation: YMLData.recommendation.cesar
-    },
-    {
-      name: "Mieko Watanabe",
-      jobPosition: "Engineer Support",
-      recommendation: YMLData.recommendation.mieko
-    },
-  ]
+  const images = {
+    "naomi": naomi,
+    "wade": wade,
+    "david": david,
+    "cesar": cesar,
+    "mieko": mieko
+  }
+  const handleRead = (e) => {
+    e.preventDefault()
+    if (e.target.innerText === 'Read more') {
+      e.target.innerText = 'Read less'
+      console.log(e.target.previousSibling.innerText)
+    }
+    else {
+      e.target.innerText = 'Read more'
+
+    }
+  }
 
   const displayRecommendations = () => {
-    return colleagues.map((colleague) => {
+    return data.colleagues.map((colleague, index) => {
+      const { testimonial } = colleague
+      const splitTestimonial = testimonial.split(' ')
+      const shortText = splitTestimonial.slice(0, 45).join(' ') + "\u2026"
+
       return (
-        <div className="testimonials">
-          <img src={Quotes} className="quotes"/>
-          <p>texto...</p>
-          <div className="info-colleague">
-            <img />
+        <div className="testimonial-wrap" key={index}>
+          <img src={Quotes} alt="quote-icon" className="quotes"/>
+          <div className="text">
+            <p className="testimonial" id={`testimonial-${index}`}>{shortText}</p>
+            <button className="cta-read" onClick={handleRead} id={`btn-${index}`}>Read more</button>
+          </div>
+          <div className="colleague-wrap">
+            <img src={images[colleague.imgName]} className="images"/>
             <div>
-              <p>{colleague.name}</p>
-              <p>{colleague.jobPosition}</p>
+              <p className="name">{colleague.name}</p>
+              <p className="position">{colleague.jobPosition}</p>
             </div>
           </div>
         </div>
@@ -50,7 +53,7 @@ export const Testimonials = () => {
     })
   }
   return (
-    <div>
+    <div className="testimonials">
       {displayRecommendations()}
     </div>
   )
